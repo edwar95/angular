5d33f861ae13e5ddebda68417da7f3325d7279c7
david <davidmoralesp1995@gmail.com>
2018-06-25 10:58:08 -0500

    angular 1.0
    interpolacion, directivas, bindings

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //interpolacion
+  titulo = 'Web';
+  usuario={
+    nombre: 'David',
+    apellido: 'Morales'
+  };
+  arregloNumeros=[5,4,3,2,1];
+  mostrarTitulo= true;
+  imageURL="https://upload.wikimedia.org/wikipedia/en/thumb/6/63/IMG_%28business%29.svg/1200px-IMG_%28business%29.svg.png";
+  imageWidth=300;
+
+  html=`
+  <h1>hola</h1>
+  <p>Hola mundo</p>
+  <img src="${this.imageURL}">  
+  `
+
+  aumentar(evento){
+    console.log('evento',evento);
+    this.imageWidth=this.imageWidth+20;
+  }
+
+  disminuir(){
+    this.imageWidth=this.imageWidth-20;
+  }
 }
